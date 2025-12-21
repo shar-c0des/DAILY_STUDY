90b325eef0ac4a20f8bf812381f7886a699e8a9a
shar-c0des <ngomakablesharleen703@gmail.com>
2025-12-21 18:05:26 +0200

    Day 1: Logic of sync execution and basic I/O

---
+++ 30-days-of-code/01.py
@@ -0,0 +1,9 @@
+# read input from stdin and save it into a variable
+# print 'Hello, World,' on the first line
+# print the contents of variable on the second line
+
+message = input()
+
+print('Hello, World')
+print(message)
+
