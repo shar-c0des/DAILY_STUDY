17c1c4a1e64006fcc628526331114ba957e6965d
shar-c0des <ngomakablesharleen703@gmail.com>
2025-12-22 17:30:52 +0200

    feat(day-01): solve data types exercise

---
+++ learning/drills/30-days-of-code/01 Data Types.py
@@ -0,0 +1,23 @@
+# initial declared variables
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# create 3 new variables by listening to the input
+i_2 = int(input())
+d_2 = float(input())
+s_2 = str(input())
+
+# add the integers
+sum_int = i + i_2  # integer sum
+
+# add the floats
+sum_float = d + d_2  # float sum
+
+# combine the strings
+combine_str = s + s_2  # combined strings
+
+# print the results on their own lines
+print(sum_int)
+print(sum_float)
+print(combine_str)
