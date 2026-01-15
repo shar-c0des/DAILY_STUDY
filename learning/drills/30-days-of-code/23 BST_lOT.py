2c242088d49b0a77e9b3eb2d8225e7325cf721a5
shar-c0des <ngomakablesharleen703@gmail.com>
2026-01-15 22:13:47 +0200

    feat(23): binary search tree level order traversal solution

---
+++ learning/drills/30-days-of-code/23 BST_lOT.py
@@ -0,0 +1,48 @@
+import sys
+from collections import deque
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        if root == None:
+            return
+
+        q = deque()
+
+
+        q.append(root)
+        curr_level = 0
+
+        while q:
+            node = q.popleft()
+            print(node.data, end=" ")
+
+            if node.left is not None:
+                q.append(node.left)
+
+            if node.right is not None:
+                q.append(node.right)
+
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
